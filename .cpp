005c5e9467f66723c63b1a55798e24b97bf5e201
trungha654 <126300569+trungha654@users.noreply.github.com>
2023-07-19 22:39:46 +0700

    Create .cpp

---
+++ .cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  std::cout << "Họ và tên:Dương Ngọc Lê Trung"<< std::endl;
+  std::cout <<"Ngày tháng năm sinh:16/3/2002"<< std::endl;
+  std::cout <<"Nghề nghiệp:sinh viên"<< std::endl;
+  std::cout <<"Mô tả bản thân:Hiền, Đô"<< std::endl;
+  return 0;
+}
