aff50e8d097788abdf8099c5ac32cdd70f66bc1d
trungha654 <126300569+trungha654@users.noreply.github.com>
2023-08-20 23:17:06 +0700

    Create .cpp

---
+++ .cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void printNewLine();
+// } Driver Code Ends
+
+//User function Template for C++
+// Function to print each word in new line
+void printNewLine(){
+  std::cout << "Hello World" << std::endl;
+}
+
+//{ Driver Code Starts.
+int main(){
+    //function call
+    printNewLine();
+}
+// } Driver Code End
