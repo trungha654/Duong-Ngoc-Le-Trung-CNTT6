296120660ceab85a35f874b47c23abf4e9b69f18
trungha654 <126300569+trungha654@users.noreply.github.com>
2023-08-20 23:20:03 +0700

    Update and rename File2.cpp to Dt2.cpp

---
+++ Dt2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void printNewLine();
+// } Driver Code Ends
+
+//User function Template for C++
+// Function to print each word in new line
+void printNewLine(){
+  std::cout << "I love the Ân" << std::endl;
+}
+
+
+//{ Driver Code Starts.
+int main(){
+    //function call
+    printNewLine();
+}
+// } Driver Code End
